25397f6635b3c8e6b09b3f0df900c6796e2c9ccb
Javier Zarate <zarate.javier.jz@gmail.com>
2020-12-14 20:23:02 -0700

    login complete

---
+++ src/spotify.js
@@ -0,0 +1,13 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+const redirectUri = 'http://localhost:3000/';
+const clientId = 'c06c97d4aa3b4d2f8c8e19ad3fb1bd51';
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
